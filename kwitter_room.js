0d0aeba7038e89bf9bb02a731b1d48462bb0bce7
Rachelooo <91844200+Rachelooo@users.noreply.github.com>
2022-04-09 09:12:50 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -11,9 +11,3 @@ var firebaseConfig = {
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
-  function adduser(){
-    username=document.getElementById("user_name").value
-    localStorage.setItem("user_name", username);
-    window.location="kwitter_room.html";
-}
